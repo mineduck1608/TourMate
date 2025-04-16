906ec81f8264bc162e7c30910e0754d5e0b3c906
Dang Minh Duc <ducdm160804@gmail.com>
2025-04-16 17:11:28 +0700

    Update next.config.ts

--- tourmatefe/next.config.ts
+++ tourmatefe/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Kích hoạt chế độ strict của React
-  basePath: '/tourmatefe', // (Nếu bạn cần thiết lập base path, ví dụ: ứng dụng chạy ở /tourmatefe)
-  images: {
-    domains: ['example.com'], // Thêm các domain hợp lệ cho hình ảnh (nếu dùng Next.js Image)
-  },
+  /* config options here */
 };
 
 export default nextConfig;
