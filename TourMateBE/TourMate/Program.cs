ad47f49315c79a8b92e95edda9e26014ff7ef2f9
Dang Minh Duc <ducdm160804@gmail.com>
2025-04-16 13:15:32 +0700

    fix dev

--- TourMateBE/TourMate/Program.cs
+++ TourMateBE/TourMate/Program.cs
@@ -11,12 +11,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
