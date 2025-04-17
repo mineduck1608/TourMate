7fdc1a6e0e1f1a355bac44e0ca0522510c3f138b
Dang Minh Duc <ducdm160804@gmail.com>
2025-04-17 12:01:27 +0700

    header

--- tourmatefe/components/header.tsx
+++ tourmatefe/components/header.tsx
@@ -1,12 +1,13 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
-import "../../styles/main.scss";
+import "@/styles/globals.css"; // Đường dẫn đến file CSS toàn cục của bạn
 import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHouse, faAngleDown } from "@fortawesome/free-solid-svg-icons";
-import logoColor from "../../images/logos/logoColor.png";
 import Link from "next/link";
 import { jwtDecode, JwtPayload } from "jwt-decode";
 import { roleJWT } from "@/types/constants";
+import Logo from "@/public/Logo.png";
+import AuthSwitcher from "@/components/AuthSwitcher";
 
 const Header = () => {
   // Giải mã JWT để kiểm tra thời gian hết hạn
@@ -84,95 +85,50 @@ const Header = () => {
       <header className={`header ${isAtTop ? "at-top" : ""}`}>
         <div className={`header-container ${isAtTop ? "large" : "small"}`}>
           <nav className="navigation">
+          <Image
+                src={Logo}
+                alt="TourMate Logo"
+                className={`logo-image ${
+                  isAtTop ? "large-logo" : "small-logo"
+                }`}
+              />
             <ul className="nav-list">
               <li>
                 <Link href="/" className="nav-link text-base">
-                  <FontAwesomeIcon icon={faHouse} className="mb-0.5 mr-3" />
-                  HOME
+                  TRANG CHỦ
                 </Link>
               </li>
               <li>
-                <a href="/about" className="nav-link text-base">
-                  ABOUT US
-                </a>
-              </li>
-              <Image
-                src={logoColor}
-                alt="Sen Spa Logo"
-                className={`logo-image ${
-                  isAtTop ? "large-logo brightness-0 invert" : "small-logo"
-                }`}
-              />
-              <li className="dropdown">
-                <a href="/media/pictures" className="nav-link text-base">
-                  MEDIA
-                  <FontAwesomeIcon
-                    icon={faAngleDown}
-                    className="mb-0.5 ml-2 text-xs"
-                  />
-                </a>
-                <ul
-                  className={`dropdown-menu min-w-[220px] rounded-br-lg rounded-tl-lg ${
-                    isAtTop ? "bg-white/20" : "small"
-                  }  backdrop-blur-sm`}
-                >
-                  <li>
-                    <a
-                      href="/media/pictures"
-                      className={`dropdown-link ${
-                        isAtTop ? "text-white" : "text-black"
-                      } group flex items-center text-base transition-transform duration-1000 hover:translate-x-2 hover:bg-transparent`}
-                    >
-                      <span className="opacity-0 transition-opacity group-hover:opacity-100">
-                        -&nbsp;
-                      </span>
-                      Pictures
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/media/videos"
-                      className={`dropdown-link ${
-                        isAtTop ? "text-white" : "text-black"
-                      } group flex items-center text-base transition-transform duration-1000 hover:translate-x-2 hover:bg-transparent`}
-                    >
-                      <span className="opacity-0 transition-opacity group-hover:opacity-100">
-                        -&nbsp;
-                      </span>
-                      Videos
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/media/e-brochure"
-                      className={`dropdown-link ${
-                        isAtTop ? "text-white" : "text-black"
-                      } group flex items-center text-base transition-transform duration-1000 hover:translate-x-2 hover:bg-transparent`}
-                    >
-                      <span className="opacity-0 transition-opacity group-hover:opacity-100">
-                        -&nbsp;
-                      </span>
-                      E-Brochure
-                    </a>
-                  </li>
-                </ul>
+                <Link href="/about" className="nav-link text-base">
+                  VỀ CHÚNG TÔI
+                </Link>
               </li>
               <li>
-                <a href="/news/blog" className="nav-link text-base">
-                  NEWS
-                </a>
+                <Link href="/about" className="nav-link text-base">
+                  TIN TỨC
+                </Link>
               </li>
               <li>
-                <a href="/contact" className="nav-link text-base">
-                  CONTACT
-                </a>
+                <Link href="/news/blog" className="nav-link text-base">
+                  TOUR
+                </Link>
               </li>
               <li>
-                <a href="/recruitment" className="nav-link text-base">
-                  RECRUITMENT
-                </a>
+                <Link href="/contact" className="nav-link text-base">
+                  BLOG
+                </Link>
+              </li>
+              <li>
+                <Link href="/recruitment" className="nav-link text-base">
+                  LIÊN HỆ
+                </Link>
               </li>
             </ul>
+            <div className={`authswitch ${
+                  isAtTop ? "large-authswitch" : "small-authswitch"
+                }`}>
+      <AuthSwitcher />
+    </div>
           </nav>
         </div>
       </header>
