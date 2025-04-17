6a69e9bda8379ee7e69b4e1caf79d3b350b65872
ngoc-main <dangngoc4332@gmail.com>
2025-04-17 10:09:31 +0700

    Scaffold with repo

---
+++ TourMateBE/Repositories/Repository/MembershipPackagesRepository.cs
@@ -0,0 +1,9 @@
+using Repositories.Models;
+using Repositories.GenericRepository;
+
+namespace Repositories.Repository
+{
+    public class MembershipPackagesRepository : GenericRepository<MembershipPackage>
+    {
+    }
+}
