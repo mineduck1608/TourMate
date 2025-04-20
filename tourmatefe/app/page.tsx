d4ebdea6a44f4ef610e87627f0cb4110c957875d
Dang Minh Duc <ducdm160804@gmail.com>
2025-04-20 17:50:26 +0700

    ư

--- tourmatefe/app/page.tsx
+++ tourmatefe/app/page.tsx
@@ -1,7 +1,7 @@
 import 'flowbite';
 import HeroSection from '@/components/HeroSection';
 import AboutSunday from '@/components/AboutSunday';
-import Header from '@/components/Header';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
