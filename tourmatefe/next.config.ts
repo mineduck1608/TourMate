876dc6d32e0be012b20e776f843a9415252f4ee1
Dang Minh Duc <ducdm160804@gmail.com>
2025-04-20 17:41:10 +0700

    sád

--- tourmatefe/next.config.ts
+++ tourmatefe/next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
-import withFlowbiteReact from "flowbite-react/plugin/nextjs";
+import type { NextConfig } from 'next';
+import withFlowbiteReact from 'flowbite-react/plugin/nextjs';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Các tùy chọn cấu hình khác của Next.js
+  webpack: (config) => {
+    // Cấu hình alias cho Vercel
+    config.resolve.alias['@'] = path.resolve(__dirname);
+
+    // Gọi lại cấu hình Webpack ban đầu
+    return config;
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
