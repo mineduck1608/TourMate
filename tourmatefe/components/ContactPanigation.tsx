0c17f6582764c330e9b3ca3bc734d909aac533a6
Dang Minh Duc <ducdm160804@gmail.com>
2025-04-21 23:41:37 +0700

    dark mode

--- tourmatefe/components/ContactPanigation.tsx
+++ tourmatefe/components/ContactPanigation.tsx
@@ -5,13 +5,13 @@ import { MailIcon } from 'lucide-react';
 
 export default function ContactPagination() {
   return (
-    <section className="bg-white dark:bg-gray-900" data-aos="fade-up"
+    <section className="bg-white" data-aos="fade-up"
     data-aos-anchor-placement="top-bottom">
       <div className="py-12 px-4 mx-auto max-w-screen-xl text-center lg:py-20">
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-tight text-gray-900 md:text-5xl lg:text-6xl">
           Kết nối với hướng dẫn viên địa phương dễ dàng hơn bao giờ hết
         </h1>
-        <p className="mb-8 text-lg font-normal text-gray-600 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+        <p className="mb-8 text-lg font-normal text-gray-600 lg:text-xl sm:px-16 lg:px-48">
           Bạn đang lên kế hoạch khám phá một điểm đến mới? Hãy để chúng tôi kết nối bạn với những hướng dẫn viên địa phương am hiểu văn hóa, ngôn ngữ và các điểm đến độc đáo – mang lại trải nghiệm du lịch chân thực và đáng nhớ nhất.
         </p>
         <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
