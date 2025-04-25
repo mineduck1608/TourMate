ac1b829603425fed7836c4b6eaa0508db94e88c3
ngoc-main <dangngoc4332@gmail.com>
2025-04-25 12:39:41 +0700

    News detail

--- tourmatefe/app/news/[id]/categories.tsx
+++ tourmatefe/app/news/[id]/categories.tsx
@@ -1,6 +1,8 @@
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
+import Link from 'next/link'
 import React from 'react'
+import { FaArrowRight } from 'react-icons/fa'
 
 export default function NewsCategories() {
     const tags = Array.from({ length: 6 }).map(
@@ -8,17 +10,29 @@ export default function NewsCategories() {
     )
     return (
         <div>
-            <ScrollArea className="h-40 rounded-md border shadow-md">
+            <ScrollArea className="h-52 rounded-md border shadow-md">
                 <div className="p-4">
                     <h4 className="mb-4 text-3xl font-medium leading-none">Danh mục</h4>
-                    {tags.map((tag) => (
-                        <div key={tag}>
-                            <div className="text-md">
-                                {tag}
-                            </div>
-                            <Separator className="my-2" />
-                        </div>
-                    ))}
+                    <table>
+                        <tbody>
+                            {tags.map((tag) => (
+                                <tr key={tag} className='*:p-1'>
+                                    <td>
+                                        <FaArrowRight />
+                                    </td>
+                                    <td className='w-full'>
+                                        <div className='py-1'>
+                                            <Link href={'#'} className="text-md flex items-center">
+                                                {tag}
+                                            </Link>
+                                            <Separator />
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
                 </div>
             </ScrollArea>
         </div>
--- tourmatefe/app/news/[id]/page.tsx
+++ tourmatefe/app/news/[id]/page.tsx
@@ -5,7 +5,7 @@ import Banner from '@/components/Banner';
 import RecentNews from './recent-news';
 import NewsCategories from './categories';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { FaFontAwesome } from 'react-icons/fa';
+import { FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
 
 export default function NewsDetailPage({
     params,
@@ -21,15 +21,34 @@ export default function NewsDetailPage({
             />
         </div>
         <div className='flex justify-between mt-10'>
-            <div className='w-[65%] p-2'>A</div>
-            <div className='w-[30%] p-2 *:mb-5'>
+            <div className='w-[70%] p-2'>A</div>
+            <div className='w-[25%] p-2 *:mb-10'>
                 <RecentNews />
                 <NewsCategories />
-                <ScrollArea className="h-80 rounded-md border shadow-md">
-                    <div className="p-4">
+                <ScrollArea className="h-60 rounded-md border shadow-md bg-black">
+                    <div className="p-4 text-white">
                         <h4 className="mb-4 text-3xl leading-none">Bạn có câu hỏi nào không?</h4>
                         <p>Đừng ngần ngại gọi cho chúng tôi. Chúng tôi là một đội ngũ chuyên gia và rất vui được trò chuyện với bạn.</p>
-                        
+                        <table className='mt-5'>
+                            <tbody>
+                                <tr className='*:p-1'>
+                                    <td>
+                                        <FaPhoneAlt fill='#ffffff' size={20} />
+                                    </td>
+                                    <td>
+                                        0974581366
+                                    </td>
+                                </tr>
+                                <tr className='*:p-1'>
+                                    <td>
+                                        <FaEnvelope fill='#ffffff' size={20} />
+                                    </td>
+                                    <td>
+                                        TourMatebooking@gmail.com
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </ScrollArea>
             </div>
