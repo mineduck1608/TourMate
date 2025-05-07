01198192d7aeefcc3e7ffe2846fb9418e52151f1
Dang Minh Duc <ducdm160804@gmail.com>
2025-05-07 23:16:32 +0700

    Update firebaseConfig.ts

--- tourmatefe/firebaseConfig.ts
+++ tourmatefe/firebaseConfig.ts
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getStorage } from 'firebase/storage';
+import { initializeApp } from 'firebase/app'
+import { getStorage } from 'firebase/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Get Firebase configuration from environment variables
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+  apiKey: 'AIzaSyAnrdte2F_VtOXJjqaq4m7cUgzIvYe3MaU',
+  authDomain: 'badmintoncourtbooking-183b2.firebaseapp.com',
+  databaseURL: 'https://badmintoncourtbooking-183b2-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'badmintoncourtbooking-183b2',
+  storageBucket: 'badmintoncourtbooking-183b2.appspot.com',
+  messagingSenderId: '859078707099',
+  appId: '1:859078707099:web:185eae36409c2810be833e',
+  measurementId: 'G-51XHF6B2B5'
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+const app = initializeApp(firebaseConfig)
+export const storage = getStorage(app)
