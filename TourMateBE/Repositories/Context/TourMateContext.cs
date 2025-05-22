38bbc79384bfa94e80e3c7138afb211e53bb9932
mineduck1608 <ducdm160804@gmail.com>
2025-05-22 23:39:29 +0700

    Update TourMateContext.cs

--- TourMateBE/Repositories/Context/TourMateContext.cs
+++ TourMateBE/Repositories/Context/TourMateContext.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Repositories.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Repositories.Context;
 
@@ -62,9 +63,19 @@ public partial class TourmateContext : DbContext
 
     public virtual DbSet<TourService> TourServices { get; set; }
 
+    public static string GetConnectionString(string connectionStringName)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string connectionString = config.GetConnectionString(connectionStringName);
+        return connectionString;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=115.73.218.193,1433;Initial Catalog=tourmate;Persist Security Info=False;User ID=sa;Password=3rd_Musketeers@@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer(GetConnectionString("DefaultConnection")).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
