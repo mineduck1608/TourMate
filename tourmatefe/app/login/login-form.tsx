3846888726034c7ce45d4e2976e5cebc007f2686
mineduck1608 <ducdm160804@gmail.com>
2025-05-26 15:56:09 +0700

    up

--- tourmatefe/app/login/login-form.tsx
+++ tourmatefe/app/login/login-form.tsx
@@ -90,7 +90,7 @@ export function LoginForm({
                 <div className="flex items-center">
                   <Label htmlFor="password">Mật Khẩu</Label>
                   <Link
-                    href="#"
+                    href="/reset-password/request"
                     className="ml-auto text-sm underline-offset-2 hover:underline"
                   >
                     Quên mật khẩu?
