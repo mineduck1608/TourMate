25fb41f82d638c394fe561d879a5cbab2cfdfb76
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-05-26 23:16:44 +0700

    Update tour guide detail on customer

--- tourmatefe/app/services/tour-guide/[id]/page.tsx
+++ tourmatefe/app/services/tour-guide/[id]/page.tsx
@@ -2,10 +2,12 @@
 import { getTourGuide } from '@/app/api/tour-guide.api';
 import Banner from '@/components/Banner';
 import { useQuery } from '@tanstack/react-query';
-import React, { use } from 'react'
-import { FaClock, FaMapMarker, FaSuitcase } from 'react-icons/fa';
+import React, { use, useState } from 'react'
+import { FaMapMarkerAlt, FaPhoneAlt, FaRegClock, FaRegMap, FaRegUser, FaSuitcaseRolling } from 'react-icons/fa';
 import TourServices from './services';
 import { TourGuide } from '@/types/tour-guide';
+import { Button } from '@/components/ui/button';
+import dayjs from 'dayjs';
 export default function TourGuideDetail({
     params,
 }: {
@@ -19,34 +21,57 @@ export default function TourGuideDetail({
         staleTime: 24 * 3600 * 1000,
     })
     const tourGuide = tourGuideData.data?.data
-
+    const [displayDesc, setDisplayDesc] = useState(true)
     return (
         <div className='*:my-10'>
             <Banner imageUrl='/tour-guide-list-banner.png' title='THÔNG TIN HƯỚNG DẪN VIÊN' />
-
-            <div className='flex justify-between p-5 shadow-lg w-[85%] rounded-lg place-self-center'>
-                <img src={tourGuide?.image || "/fallback.jpg"}
-                    alt={tourGuide?.fullName}
-                    className="w-[30%] h-60 object-cover border-2" />
-                <div className='w-[65%]'>
-                    <h4 className="font-semibold text-3xl text-gray-800 p-2 mb-4">
-                        {tourGuide?.fullName}
-                    </h4>
-                    {tourGuide && <table>
-                        <tbody>
-                            {
-                                statToRender(tourGuide).map((v, i) => (
-                                    <tr key={i}>
-                                        <td className='p-2'>{v.icon}</td>
-                                        <td>{v.value}</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>}
-                    <p className='p-2'>
-                        {tourGuide?.tourGuideDescs?.[0].description}
-                    </p>
+            <div className='shadow-lg w-[85%] rounded-lg place-self-center'>
+                <div className='flex justify-between p-5 '>
+                    <img src={tourGuide?.image || "/fallback.jpg"}
+                        alt={tourGuide?.fullName}
+                        className="w-[30%] h-60 object-cover border-2" />
+                    <div className='w-[65%]'>
+                        <h4 className="font-semibold text-3xl text-gray-800 p-2 mb-4">
+                            {tourGuide?.fullName}
+                        </h4>
+                        {tourGuide && <table>
+                            <tbody>
+                                {
+                                    statToRender(tourGuide).map((v, i) => (
+                                        <tr key={i}>
+                                            <td className='p-2'>
+                                                <span className='flex gap-5 font-bold'>{v.icon}{v.name}</span>
+                                            </td>
+                                            <td>{v.value}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>}
+                    </div>
+                </div>
+                <div className='pt-2 px-5'>
+                    <div className='flex justify-between'>
+                        <p className='text-2xl font-bold'>Mô tả</p>
+                        <Button
+                            onClick={() => setDisplayDesc(p => !p)}
+                            className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 focus:outline-none cursor-pointer mb-2.5'>
+                            {displayDesc ? 'Ẩn' : 'Hiện'}
+                        </Button>
+                    </div>
+                    <div
+                        className={`text-justify ${displayDesc ? 'block' : 'hidden'}`}
+                        dangerouslySetInnerHTML={{
+                            __html: tourGuide?.tourGuideDescs?.[0].description
+                                ? tourGuide?.tourGuideDescs?.[0].description.replace(
+                                    /(https?:\/\/.*\.(?:png|jpg|jpeg|gif|bmp|svg))/gi,
+                                    (match) => {
+                                        return `<img src="${match}" alt="Image" style="max-width: 100%; max-height: 100%; object-fit: contain;" />`;
+                                    }
+                                )
+                                : "",
+                        }}
+                    />
                 </div>
             </div>
             <div className='flex justify-between p-5 shadow-lg w-[85%] rounded-lg place-self-center'>
@@ -58,15 +83,33 @@ export default function TourGuideDetail({
 
 const statToRender = (t: TourGuide) => [
     {
-        icon: <FaMapMarker />,
-        value: t.tourGuideDescs?.[0].area.areaName
+        icon: <FaRegMap size={25} />,
+        value: t.tourGuideDescs?.[0].area.areaName,
+        name: 'Địa điểm hoạt động'
     },
     {
-        icon: <FaClock />,
-        value: t.dateOfBirth
+        icon: <FaRegClock size={25} />,
+        value: dayjs(t.dateOfBirth).format('DD/MM/YYYY'),
+        name: 'Ngày sinh'
     },
     {
-        icon: <FaSuitcase />,
-        value: t.tourGuideDescs?.[0].yearOfExperience + ' năm kinh nghiệm'
+        icon: <FaSuitcaseRolling size={25} />,
+        value: t.tourGuideDescs?.[0].yearOfExperience ?? '',
+        name: 'Số năm kinh nghiệm'
+    },
+    {
+        icon: <FaRegUser size={25} />,
+        value: t.gender,
+        name: 'Giới tính'
+    },
+    {
+        icon: <FaMapMarkerAlt size={25} />,
+        value: t.address,
+        name: 'Địa chỉ'
+    },
+    {
+        icon: <FaPhoneAlt size={25} />,
+        value: t.phone,
+        name: 'Số điện thoại'
     }
 ]
