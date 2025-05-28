8322afa947f284a940479cad62dfdc3b5cebf0d2
mineduck1608 <ducdm160804@gmail.com>
2025-05-28 15:55:32 +0700

    fixx

--- tourmatefe/app/services/tour-guide/[id]/page.tsx
+++ tourmatefe/app/services/tour-guide/[id]/page.tsx
@@ -68,7 +68,7 @@ export default function TourGuideDetail({
                         </Button>
                     </div>
                     <div
-                        className={`text-justify ${displayDesc ? 'block' : 'hidden'}`}
+                        className={`text-justify ${displayDesc ? 'block mb-10' : 'hidden'}`}
                         dangerouslySetInnerHTML={{
                             __html: tourGuide?.tourGuideDescs?.[0]?.description
                                 ? tourGuide?.tourGuideDescs?.[0]?.description.replace(
