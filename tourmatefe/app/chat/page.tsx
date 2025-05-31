4f37703d1dad764629f4e3f3d1fd8b974a7fb422
mineduck1608 <ducdm160804@gmail.com>
2025-05-31 22:28:55 +0700

    fixx error

--- tourmatefe/app/chat/page.tsx
+++ tourmatefe/app/chat/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ConversationList from "./conversationList";
 import MessageList from "./messageList";
 import MegaMenu from "@/components/mega-menu";
