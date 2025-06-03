613bd5424c234de32545c584c73b0cb7bb462ced
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-06-03 22:13:56 +0700

    Add overlay

--- tourmatefe/app/tour-guide/profile/profile-components/services.tsx
+++ tourmatefe/app/tour-guide/profile/profile-components/services.tsx
@@ -11,7 +11,6 @@ import { ServiceEditContext, ServiceEditContextProp } from './service-edit-conte
 import { TourService } from '@/types/tour-service'
 import { toast } from 'react-toastify'
 import { TourGuideSiteContext, TourGuideSiteContextProps } from '../../layout'
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
 
 export default function TourServices({ tourGuideId }: { tourGuideId: number | string }) {
     const [page, setPage] = useState(1)
@@ -100,46 +99,48 @@ export default function TourServices({ tourGuideId }: { tourGuideId: number | st
                     className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
                 >
                     {services.map((item) => (
-                        <Tooltip key={item.serviceId}>
-                            <TooltipTrigger>
-                                <motion.div
-                                    whileHover={{
-                                        scale: 1.05,
-                                    }}
-                                    onClick={() => {
-                                        setTarget(item)
-                                        setTimeout(() => {
-                                            setModalOpen({ ...modalOpen, edit: true })
-                                        }, 50);
-                                    }}
-                                    transition={{ duration: 0.1, ease: "easeInOut" }}
-                                    className="bg-white rounded-xl shadow-sm overflow-hidden cursor-pointer transform transition-all"
-                                >
+                        <div key={item.serviceId}>
+                            <motion.div
+                                whileHover={{
+                                }}
+                                onClick={() => {
+                                    setTarget(item)
+                                    setTimeout(() => {
+                                        setModalOpen({ ...modalOpen, edit: true })
+                                    }, 50);
+                                }}
+                                transition={{ duration: 0.1, ease: "easeInOut" }}
+                                className="bg-white rounded-xl shadow-sm overflow-hidden cursor-pointer transform transition-all relative"
+                            >
+                                <div className="relative">
                                     <SafeImage
                                         src={item.image}
                                         alt={item.serviceName}
                                         className="w-full h-70 object-cover"
                                     />
-                                    <div className="flex justify-between">
-                                        <div className="p-6">
-                                            <p className="text-sm text-gray-500 mb-1">{dayjs(item.createdDate).format('DD/MM/YYYY HH:mm:ss')}</p>
-                                            <h3 className="font-semibold text-lg mb-2">{item.serviceName}</h3>
-                                            {/* <p className="text-sm text-gray-700">{item.}</p> */}
-                                        </div>
-                                        <div className="relative content-center">
-                                            <Link
-                                                href={'/news/' + item.serviceId}
-                                                className="text-nowrap text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-                                                Xem ngay
-                                            </Link>
-                                        </div>
+                                    <motion.div
+                                        initial={{ opacity: 0 }}
+                                        whileHover={{ opacity: 1 }}
+                                        className="absolute inset-0 bg-[rgba(0,0,0,0.25)] bg-opacity-50 flex items-center justify-center"
+                                    >
+                                        <span className="text-white text-xl font-semibold">Cập nhật</span>
+                                    </motion.div>
+                                </div>
+                                <div className="flex justify-between">
+                                    <div className="p-6">
+                                        <p className="text-sm text-gray-500 mb-1">{dayjs(item.createdDate).format('DD/MM/YYYY HH:mm:ss')}</p>
+                                        <h3 className="font-semibold text-lg mb-2">{item.serviceName}</h3>
                                     </div>
-                                </motion.div>
-                            </TooltipTrigger>
-                            <TooltipContent>
-                                <p>Cập nhật</p>
-                            </TooltipContent>
-                        </Tooltip>
+                                    <div className="relative content-center">
+                                        <Link
+                                            href={'/news/' + item.serviceId}
+                                            className="text-nowrap text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+                                            Xem ngay
+                                        </Link>
+                                    </div>
+                                </div>
+                            </motion.div>
+                        </div>
                     ))}
                 </motion.div>
             </AnimatePresence>
