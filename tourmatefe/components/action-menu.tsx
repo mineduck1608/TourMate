c238aad9a671d694eed1362d20f57b2fb9f7b14e
mineduck1608 <ducdm160804@gmail.com>
2025-06-23 15:42:46 +0700

    revenue path

--- tourmatefe/components/action-menu.tsx
+++ tourmatefe/components/action-menu.tsx
@@ -9,7 +9,7 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "./ui/button";
-import { Settings, LogOut, MapPin, CalendarCheck2, PackageOpen, History } from "lucide-react";
+import { Settings, LogOut, MapPin, CalendarCheck2, PackageOpen, History, Wallet } from "lucide-react";
 import { useToken } from "./getToken";
 import { MyJwtPayload } from "@/types/JwtPayload";
 import { jwtDecode } from "jwt-decode";
@@ -87,6 +87,13 @@ export default function ActionMenu() {
               >
                 <PackageOpen size={18} />
                 Gói thành viên
+              </Link>
+              <Link
+                href={`/tour-guide/revenue`}
+                className="w-full flex items-center gap-3 text-gray-800 hover:bg-gray-100 px-4 py-2 rounded-md"
+              >
+                <Wallet size={18} />
+                Doanh thu
               </Link>
             </>
           ) : (
