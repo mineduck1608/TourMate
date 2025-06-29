9bacb1c3c71147d86ae9ffab48aa08b5c741d49e
mineduck1608 <ducdm160804@gmail.com>
2025-06-29 22:09:41 +0700

    update id for service update

--- tourmatefe/app/tour-guide/profile/profile-components/edit-service-modal.tsx
+++ tourmatefe/app/tour-guide/profile/profile-components/edit-service-modal.tsx
@@ -173,7 +173,7 @@ function ServiceEditModal({ isOpen, onClose }: Props) {
                             type="text"
                             name="title"
                             placeholder="Nhập tiêu đề"
-                            id="duration"
+                            id="title"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             value={target.title}
                             onChange={handleChange}
@@ -190,7 +190,7 @@ function ServiceEditModal({ isOpen, onClose }: Props) {
                         <input
                             type="text"
                             name="tourDesc"
-                            id="duration"
+                            id="tourDesc"
                             placeholder="Nhập mô tả"
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             value={target.tourDesc}
