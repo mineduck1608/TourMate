891f024f86fb0a0e4bb13c8ecd29dad6f83242ca
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-07-02 11:57:22 +0700

    Update login-form.tsx

--- tourmatefe/app/login/login-form.tsx
+++ tourmatefe/app/login/login-form.tsx
@@ -158,7 +158,7 @@ export function LoginForm({
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
                 <span className="relative z-10 bg-background px-2 text-muted-foreground">Hoặc tiếp tục với</span>
               </div>
-              <Button onClick={handleGoogleLogin} variant="outline" className="w-full" disabled={loading}>
+              <Button onClick={handleGoogleLogin} variant="outline" className="w-full" disabled={loading} type="button">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="mr-2 h-4 w-4">
                   <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
                   <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
